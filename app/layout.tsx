14a4d924c51c288fb84564ea3c58e80e791dda25
Faisal Hasan <gigagrowthwebsolutions@gmail.com>
2026-09-09 23:16:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "GigaGrowth Web Solutions - Custom Web & Digital Marketing",
   description:
-    "GigaGrowth Web Solutions provides tailored web design, app development, and digital marketing services to boost your online presence and drive revenue.",
-    generator: 'v0.app'
+    "GigaGrowth Web Solutions provides tailored web design, app development, and digital marketing services to boost your online presence and drive revenue."
 }
 
 export default function RootLayout({
